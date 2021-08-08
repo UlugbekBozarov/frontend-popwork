278525e4eb399bcc5e46d2abd886fb3d004e2963
UlugbekBozarov <ulugbek01081999@gmail.com>
2021-08-08 20:12:19 +0500

    navbar

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import LandingPage from './screen/LandingPage';
+
+function App() {
+  return (
+    <div className="app">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/screen/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../component/shared/Navbar'
+
+const LandingPage = () => {
+    return (
+        <div style={{paddingTop: "100px"}}>
+            <Navbar />
+        </div>
+    )
+}
+
+export default LandingPage
